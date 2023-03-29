a1fc7000ddf438a2b7398a1a6ddbdac63ce5dc7d
saihon <devqrx+saihon-github@gmail.com>
2023-03-29 23:50:20 +0900

    Add the feature add bookmarks with shortcut keys.

--- js/background.js
+++ js/background.js
@@ -1,32 +1,41 @@
-'use strict';
+"use strict";
 
 // What to do when starting the browser
 const ObeyDisabledSettings = true;
 bookmarks.remove(ObeyDisabledSettings);
 
-const getCurrentTab = tabs => {
-    if (tabs.length == 0) {
-        return;
-    }
+const addBookmark = (tabs) => {
+  if (tabs.length == 0) {
+    return;
+  }
 
-    const tab  = tabs[0];
-    const info = {index : 0, parentId : '', title : tab.title, url : tab.url};
+  const tab = tabs[0];
+  const info = { index: 0, parentId: "", title: tab.title, url: tab.url };
 
-    storages.get(item => {
-        if (item.settings.folderId == '') {
-            folders.create(folderId => {
-                info.parentId = folderId
-                bookmarks.create(info, tab.id);
-            });
-            return;
-        }
-        info.parentId = item.settings.folderId;
+  storages.get((item) => {
+    if (typeof item == "undefined" || item.settings.folderId == "") {
+      folders.create((folderId) => {
+        info.parentId = folderId;
         bookmarks.create(info, tab.id);
-    });
+      });
+      return;
+    }
+    info.parentId = item.settings.folderId;
+    bookmarks.create(info, tab.id);
+  });
 };
 
 const onClicked = () => {
-    chrome.tabs.query({currentWindow : true, active : true}, getCurrentTab);
+  chrome.tabs.query({ currentWindow: true, active: true }, addBookmark);
 };
 
+const onCommand = (command) => {
+  switch (command) {
+    case "add-bookmark":
+      onClicked();
+      break;
+  }
+};
+
+chrome.commands.onCommand.addListener(onCommand);
 chrome.browserAction.onClicked.addListener(onClicked);
--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,141 +1,144 @@
 
 // https://developer.chrome.com/docs/extensions/reference/storage/
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage
-const storages = (new class Storages {
-    constructor() {
-        this._KEY              = 'settings';
-        this._DEFAULT_SETTINGS = Object.freeze({
-            day : 30, // 1 month
-            folderId : '',
-            disable : false,
-        });
+const storages = new (class Storages {
+  constructor() {
+    this._KEY = "settings";
+    this._DEFAULT_SETTINGS = {
+      day: 30, // 1 month
+      folderId: "",
+      disable: false,
+    };
 
-        // local or sync
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage#properties
-        this._PROP_NAME = 'sync';
-    }
+    // local or sync
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage#properties
+    this._PROP_NAME = "sync";
+  }
 
-    get(callback) {
-        const self = this;
-        chrome.storage[self._PROP_NAME].get(self._KEY, (item) => {
-            if (!item[self._KEY]) {
-                item[self._KEY] = self._DEFAULT_SETTINGS;
-                self.set(item);
-            }
+  get(callback) {
+    const self = this;
+    chrome.storage[self._PROP_NAME].get(self._KEY, (item) => {
+      if (typeof item == 'undefined' || !item[self._KEY]) {
+        item = {};
+        item[self._KEY] = self._DEFAULT_SETTINGS;
+        self.set(item);
+      }
 
-            // "settings.day" is a new property, so may not have.
-            if (!item[self._KEY]['day']) {
-                item['day'] = self._DEFAULT_SETTINGS.day;
-                self.set(item);
-            }
+      // "settings.day" is a new property, so may not have.
+      if (!item[self._KEY]["day"]) {
+        item["day"] = self._DEFAULT_SETTINGS.day;
+        self.set(item);
+      }
 
-            if (typeof callback != 'undefined') callback(item);
-        });
-    }
+      if (typeof callback != "undefined") callback(item);
+    });
+  }
 
-    set(item) {
-        chrome.storage[this._PROP_NAME].set(item);
-    }
-}());
+  set(item) {
+    chrome.storage[this._PROP_NAME].set(item);
+  }
+})();
 
 // https://developer.chrome.com/docs/extensions/reference/bookmarks/
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks
-const folders = (new class Folders {
-    constructor() {
-        this._NAME = 'Temporary Bookmarks';
+const folders = new (class Folders {
+  constructor() {
+    this._NAME = "Temporary Bookmarks";
+  }
+
+  create(callback) {
+    const self = this;
+    // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-create
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/CreateDetails
+    // If url is NULL or missing, it will be a folder.
+    chrome.bookmarks.create({ title: self._NAME, index: 0 }, (results) => {
+      storages.get((item) => {
+        item.settings.folderId = results.id;
+        storages.set(item);
+        if (typeof callback != "undefined") callback(item.settings.folderId);
+      });
+    });
+  }
+
+  walk(folderId, timeInMs, expirationMs, fn) {
+    if (!folderId) {
+      return;
     }
 
-    create(callback) {
-        const self = this;
-        // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-create
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/CreateDetails
-        // If url is NULL or missing, it will be a folder.
-        chrome.bookmarks.create({title : self._NAME, index : 0}, results => {
-            storages.get(item => {
-                item.settings.folderId = results.id;
-                storages.set(item);
-                if (typeof callback != 'undefined')
-                    callback(item.settings.folderId);
-            });
-        });
-    }
+    const self = this;
 
-    walk(folderId, timeInMs, expirationMs, fn) {
-        if (!folderId) {
-            return;
+    // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-getChildren
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/getChildren
+    chrome.bookmarks.getChildren(folderId, (children) => {
+      // when not an error, runtime.lastError is undefined in chrome
+      // and null in firefox
+      if (chrome.runtime.lastError) {
+        return;
+      }
+
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/BookmarkTreeNode
+      // node is created BookmarkTreeNode.
+      // Verified that node.dateAdded and Date.now() return the same
+      // value. console.log("dateAdded:", node.dateAdded);
+      // console.log("Date now :", Date.now());
+
+      for (let c of children) {
+        // If child is a folder,
+        // url is `undefined` on both chrome and firefox.
+        if (typeof c["url"] == "undefined") {
+          self.walk(c.id, timeInMs, expirationMs, fn);
+          continue;
         }
 
-        const self = this;
-
-        // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-getChildren
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/getChildren
-        chrome.bookmarks.getChildren(folderId, children => {
-            // when not an error, runtime.lastError is undefined in chrome
-            // and null in firefox
-            if (chrome.runtime.lastError) {
-                return;
-            }
-
-            // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/BookmarkTreeNode
-            // node is created BookmarkTreeNode.
-            // Verified that node.dateAdded and Date.now() return the same
-            // value. console.log("dateAdded:", node.dateAdded);
-            // console.log("Date now :", Date.now());
-
-            for (let c of children) {
-                // If child is a folder,
-                // url is `undefined` on both chrome and firefox.
-                if (typeof c['url'] == 'undefined') {
-                    self.walk(c.id, timeInMs, expirationMs, fn);
-                    continue;
-                }
-
-                let elapsedMs = timeInMs - c.dateAdded;
-                if (elapsedMs > expirationMs) {
-                    fn(c.id);
-                }
-            }
-        });
-    }
-}());
+        let elapsedMs = timeInMs - c.dateAdded;
+        if (elapsedMs > expirationMs) {
+          fn(c.id);
+        }
+      }
+    });
+  }
+})();
 
 // https://developer.chrome.com/docs/extensions/reference/browserAction/#badge
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction
 const BADGE_COLOR = Object.freeze({
-    blue : [ 0, 51, 204, 255 ],
-    yellow : [ 255, 255, 60, 255 ],
+  blue: [0, 51, 204, 255],
+  yellow: [255, 255, 60, 255],
 });
 
 function badgeText(details) {
-    // https://developer.chrome.com/docs/extensions/reference/browserAction/#method-setBadgeText
-    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction/setBadgeText
-    chrome.browserAction.setBadgeText(details);
+  // https://developer.chrome.com/docs/extensions/reference/browserAction/#method-setBadgeText
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction/setBadgeText
+  chrome.browserAction.setBadgeText(details);
 }
 
 function badgeIncrement() {
-    // https://developer.chrome.com/docs/extensions/reference/browserAction/#method-getBadgeText
-    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction/getBadgeText
-    chrome.browserAction.getBadgeText(
-        {}, text => { badgeText({text : ((text - 0) + 1) + ''}); });
+  // https://developer.chrome.com/docs/extensions/reference/browserAction/#method-getBadgeText
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction/getBadgeText
+  chrome.browserAction.getBadgeText({}, (text) => {
+    badgeText({ text: text - 0 + 1 + "" });
+  });
 }
 
 function badgeColor(details) {
-    // https://developer.chrome.com/docs/extensions/reference/browserAction/#method-setBadgeBackgroundColor
-    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction/setBadgeBackgroundColor
-    chrome.browserAction.setBadgeBackgroundColor(details);
+  // https://developer.chrome.com/docs/extensions/reference/browserAction/#method-setBadgeBackgroundColor
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction/setBadgeBackgroundColor
+  chrome.browserAction.setBadgeBackgroundColor(details);
 }
 
 function badgeReset(details, timeout) {
-    if (!details) {
-        details = {};
-    }
-    details['text'] = '';
+  if (!details) {
+    details = {};
+  }
+  details["text"] = "";
 
-    if (timeout) {
-        setTimeout(() => {badgeText(details)}, timeout);
-        return;
-    }
-    badgeText(details);
+  if (timeout) {
+    setTimeout(() => {
+      badgeText(details);
+    }, timeout);
+    return;
+  }
+  badgeText(details);
 }
 
 const DAY_MILLISECONDS = 86400000; // 1 day millisecond
@@ -144,72 +147,74 @@ const BADGE_RESET_MILLISECONDS = 1000 * 60; // 1 min
 
 // https://developer.chrome.com/docs/extensions/reference/bookmarks/
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks
-const bookmarks = (new class Bookmarks {
-    constructor() {}
+const bookmarks = new (class Bookmarks {
+  constructor() {}
 
-    _removeOne(id) {
-        // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-remove
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/remove
-        chrome.bookmarks.remove(id, () => {
-            if (chrome.runtime.lastError) return;
-            badgeIncrement();
+  _removeOne(id) {
+    // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-remove
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/remove
+    chrome.bookmarks.remove(id, () => {
+      if (chrome.runtime.lastError) return;
+      badgeIncrement();
+    });
+  }
+
+  // obeyDisabledSettings: specify false when deleting with the "Delete"
+  // button on the setting page.
+  remove(obeyDisabledSettings) {
+    const self = this;
+
+    // Delete expired bookmarks.
+    storages.get((item) => {
+      if (obeyDisabledSettings && item.settings.disable) {
+        return;
+      }
+
+      if (item.settings.folderId == "") {
+        return;
+      }
+
+      badgeColor({ color: BADGE_COLOR.blue });
+
+      folders.walk(
+        item.settings.folderId,
+        Date.now(),
+        item.settings.day * DAY_MILLISECONDS,
+        self._removeOne
+      );
+
+      badgeReset(null, BADGE_RESET_MILLISECONDS);
+    });
+  }
+
+  create(info, tabId, recovered) {
+    const self = this;
+    // If not specify info.index in firefox, it will be in alphabet order,
+    // so get the children length, for the add to the end of the folder.
+    chrome.bookmarks.getChildren(info.parentId, (children) => {
+      // If an error occurs, recover only once.
+      // It error is probably due to can not be find the folder id.
+      if (!recovered && chrome.runtime.lastError) {
+        folders.create((folderId) => {
+          info.parentId = folderId;
+          self.create(info, tabId, true);
         });
-    }
+        return;
+      }
 
-    // obeyDisabledSettings: specify false when deleting with the "Delete"
-    // button on the setting page.
-    remove(obeyDisabledSettings) {
-        const self = this;
+      info.index = children.length;
 
-        // Delete expired bookmarks.
-        storages.get(item => {
-            if (obeyDisabledSettings && item.settings.disable) {
-                return;
-            }
+      // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-create
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/create
+      chrome.bookmarks.create(info, (node) => {
+        if (chrome.runtime.lastError) {
+          return;
+        }
 
-            if (item.settings.folderId == '') {
-                return;
-            }
-
-            badgeColor({color : BADGE_COLOR.blue});
-
-            folders.walk(item.settings.folderId,
-                         Date.now(),
-                         item.settings.day * DAY_MILLISECONDS,
-                         self._removeOne);
-
-            badgeReset(null, BADGE_RESET_MILLISECONDS);
-        });
-    }
-
-    create(info, tabId, recovered) {
-        const self = this;
-        // If not specify info.index in firefox, it will be in alphabet order,
-        // so get the children length, for the add to the end of the folder.
-        chrome.bookmarks.getChildren(info.parentId, children => {
-            // If an error occurs, recover only once.
-            // It error is probably due to can not be find the folder id.
-            if (!recovered && chrome.runtime.lastError) {
-                folders.create(folderId => {
-                    info.parentId = folderId;
-                    self.create(info, tabId, true);
-                });
-                return;
-            }
-
-            info.index = children.length;
-
-            // https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-create
-            // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/create
-            chrome.bookmarks.create(info, node => {
-                if (chrome.runtime.lastError) {
-                    return;
-                }
-
-                badgeColor({color : BADGE_COLOR.yellow, tabId : tabId});
-                badgeText({text : '+', tabId : tabId});
-                badgeReset({tabId : tabId}, 1400);
-            });
-        });
-    }
-}());
+        badgeColor({ color: BADGE_COLOR.yellow, tabId: tabId });
+        badgeText({ text: "+", tabId: tabId });
+        badgeReset({ tabId: tabId }, 1400);
+      });
+    });
+  }
+})();
